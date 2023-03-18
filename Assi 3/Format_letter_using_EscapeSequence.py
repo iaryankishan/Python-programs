ad9486edb531d2143292e618c63dbf429710bf11
Aryan Kumar <107184052+iaryankishan@users.noreply.github.com>
2023-03-18 16:54:48 +0530

    Create Format_letter_using_EscapeSequence.py

---
+++ Assi 3/Format_letter_using_EscapeSequence.py
@@ -0,0 +1,5 @@
+letter = " Dear Aryan, This python course is nice. Thanks! "
+print(letter)
+
+formatted_letter = "Dear Aryan,\n\t This python course is nice.\n\t\t\t\tThanks!"
+print(formatted_letter)
