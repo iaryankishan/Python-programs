214c09d1aff507fc7219c927d8ed6f7bb59f165e
Aryan Kumar <107184052+iaryankishan@users.noreply.github.com>
2023-03-20 01:21:44 +0530

    Create Marks_stored_in_sorted_manner.py

---
+++ Assi 4/Marks_stored_in_sorted_manner.py
@@ -0,0 +1,11 @@
+f1= int(input("Enter mark for student number 1 "))
+f2= int(input("Enter mark for student number 2 "))
+f3= int(input("Enter mark for student number 3 "))
+f4= int(input("Enter mark for student number 4 "))
+f5= int(input("Enter mark for student number 5 "))
+f6= int(input("Enter mark for student number 6 "))
+
+
+myList = [f1,f2,f3,f4,f5,f6]
+myList.sort()
+print(myList)
