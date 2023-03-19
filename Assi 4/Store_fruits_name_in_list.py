eb6b906cd28059230be257040d12f7b32ef8bb69
Aryan Kumar <107184052+iaryankishan@users.noreply.github.com>
2023-03-20 01:10:31 +0530

    Create Store_fruits_name_in_list.py

---
+++ Assi 4/Store_fruits_name_in_list.py
@@ -0,0 +1,10 @@
+f1= input("Entered fruits number 1 ")
+f2= input("Entered fruits number 2 ")
+f3= input("Entered fruits number 3 ")
+f4= input("Entered fruits number 4 ")
+f5= input("Entered fruits number 5 ")
+f6= input("Entered fruits number 6 ")
+f7= input("Entered fruits number 7 ")
+
+myFruitsList = [f1,f2,f3,f4,f5,f6,f7]
+print(myFruitsList)
