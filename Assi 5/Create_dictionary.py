8bdf28e1002f900e2918c829f61a7d3745d2c620
Aryan Kumar <107184052+iaryankishan@users.noreply.github.com>
2023-03-21 17:52:58 +0530

    Create Create_dictionary.py

---
+++ Assi 5/Create_dictionary.py
@@ -0,0 +1,9 @@
+Dict = {
+    "Khana" : "Food",
+    "Paani" : "Water",
+    "Ghar" : "House",
+    "Paisa" : "Money"
+}
+print("Choose from ", Dict.keys())
+a= input("Enter the Hindi word\n")
+print("Meaning of the word is: ",Dict[a])
