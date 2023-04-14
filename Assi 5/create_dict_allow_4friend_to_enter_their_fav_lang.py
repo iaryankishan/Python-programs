8ef8b58b8f46146bfeb0fe702ebd76c6a7caa395
Aryan Kumar <107184052+iaryankishan@users.noreply.github.com>
2023-04-14 18:37:30 +0530

    Create create_dict_allow_4friend_to_enter_their_fav_lang.py

---
+++ Assi 5/create_dict_allow_4friend_to_enter_their_fav_lang.py
@@ -0,0 +1,11 @@
+FavLang = {}
+a=input("Enter your favorite language Aryan\n")
+b=input("Enter your favorite language Ayush\n")
+c=input("Enter your favorite language Viraj\n")
+d=input("Enter your favorite language Prashant\n")
+
+FavLang['Aryan']=a
+FavLang['Ayush']=b
+FavLang['Viraj']=c
+FavLang['Prashant']=d
+print(FavLang)
