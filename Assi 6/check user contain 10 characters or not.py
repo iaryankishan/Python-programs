ca41afb9ed646d361c1f21edaf2be41e7e147b0f
Aryan Kumar <107184052+iaryankishan@users.noreply.github.com>
2023-04-21 11:44:43 +0530

    Create check user contain 10 characters or not.py

---
+++ Assi 6/check user contain 10 characters or not.py
@@ -0,0 +1,8 @@
+username=input("Enter the username\n")
+l=len(username)
+if(l==10):
+ print("username contain 10 charactes")
+elif(l>10):
+ print("username contain more than 10 charactes")
+else:
+  print("username contain less than 10 charactes")
