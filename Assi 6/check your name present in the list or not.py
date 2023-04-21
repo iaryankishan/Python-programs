e61a3477afa1ddcc68719c76df2553bb9b43ef9c
Aryan Kumar <107184052+iaryankishan@users.noreply.github.com>
2023-04-21 11:52:17 +0530

    Create check your name present in the list or not.py

---
+++ Assi 6/check your name present in the list or not.py
@@ -0,0 +1,6 @@
+names=["aryan","ayush","viraj","prashant","utkarsh"]
+name=input("Enter the name to check\n")
+if name in names:
+    print("Your name is prestent in the list")
+else:
+    print("Your name is not present in the list")
