ec329eccabe81a1a095e9755b261c223438d39ab
Aryan Kumar <107184052+iaryankishan@users.noreply.github.com>
2023-04-21 12:05:55 +0530

    Create check the post is talking about you or noy.py

---
+++ Assi 6/check the post is talking about you or noy.py
@@ -0,0 +1,8 @@
+post=input("Enter any post\n")
+
+if("Aryan" in post):
+    print("This post is talking about Aryan")
+elif("aryan" in post):
+    print("This post is talking about Aryan")
+else:
+    print("This post is not talking about Aryan")
